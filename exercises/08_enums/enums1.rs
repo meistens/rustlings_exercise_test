cbebf6d6824195c6d2382d9baf85cc3e85f85f0e
David Mebo <mebodave@gmail.com>
2025-03-25 20:54:16 +0100

    Add enum variants and implement message processing

--- exercises/08_enums/enums1.rs
+++ exercises/08_enums/enums1.rs
@@ -1,12 +1,17 @@
 #[derive(Debug)]
 enum Message {
     // TODO: Define a few types of messages as used below.
+    Resize(u32, u32),
+    Move(i32, i32),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit,
 }
 
 fn main() {
-    println!("{:?}", Message::Resize);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Resize(100, 200));
+    println!("{:?}", Message::Move(10, 20));
+    println!("{:?}", Message::Echo("Hello".to_string()));
+    println!("{:?}", Message::ChangeColor(255, 0, 0));
     println!("{:?}", Message::Quit);
 }
